a605abeee2380685f205930067e81a48dc8a696e
WaldReznov <>
2019-03-13 19:39:00 +0300

    Complete front-end
    13.03.2019
    9 work hours

---
+++ script/script.js
@@ -0,0 +1,18 @@
+document.getElementById('burger').onclick=function(){
+link();
+};
+function link(){
+  document.getElementById('burger').classList.toggle('menu-link-active');
+  document.getElementById('nav-ul').classList.toggle('nav-ul-link');
+}
+var buttonClick = document.getElementsByClassName('button-modal');
+for (i=0; i< buttonClick.length; i++)
+buttonClick[i].onclick = function()
+{ modal();
+};
+function modal(){
+document.getElementById("modal-window").classList.toggle("modal-active");
+};
+document.getElementById("modal").onclick=function(){
+ document.getElementById("modal-window").classList.remove("modal-active");
+};
